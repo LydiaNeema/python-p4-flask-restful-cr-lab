354bf5c137f50e96e4316f876965dba97806e0d2
Lydia Neema <lydianeema25@gmail.com>
2025-09-16 10:25:38 +0300

    updated test

--- server/app.py
+++ server/app.py
@@ -3,7 +3,6 @@
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
-
 from models import db, Plant
 
 app = Flask(__name__)
@@ -13,15 +12,41 @@ app.json.compact = True
 
 migrate = Migrate(app, db)
 db.init_app(app)
-
 api = Api(app)
 
+class Home(Resource):
+    def get(self):
+        return make_response(jsonify({"message": "Welcome to the Plant Store API"}), 200)
+
+api.add_resource(Home, '/')
+
+
 class Plants(Resource):
-    pass
+    def get(self):
+        response_dict_list = [plant.to_dict() for plant in Plant.query.all()]
+        return make_response(jsonify(response_dict_list), 200)
+
+    def post(self):
+        data = request.get_json()
+        new_plant = Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price']
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+        return make_response(jsonify(new_plant.to_dict()), 201)
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant = Plant.query.filter(Plant.id == id).first()
+        if plant:
+            return make_response(jsonify(plant.to_dict()), 200)
+        else:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
